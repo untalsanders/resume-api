eafbf3f08e12659a4cbdf60cf94b8fdf3795736f
Sanders Gutiérrez <ing.sanders@gmail.com>
2023-04-20 07:32:22 -0300

    fix: fixed greeting tests
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/test/java/com/sandersgutierrez/resume/domain/service/GreetingServiceTests.java
+++ src/test/java/com/sandersgutierrez/resume/domain/service/GreetingServiceTests.java
@@ -1,22 +1,14 @@
 package com.sandersgutierrez.resume.domain.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
-@SpringBootTest
 class GreetingServiceTests {
-
-    @MockBean
-    private GreetingService service;
 
     @Test
     void greetShouldReturnMessageOfGreeting() {
-        when(service.greet("Sanders")).thenReturn("Hello Sanders!");
+        GreetingService service = new GreetingService();
         assertEquals("Hello Sanders!", service.greet("Sanders"));
-        verify(service, times(1)).greet("Sanders");
     }
 }
