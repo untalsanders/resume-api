34568f50fb3811c67038d4b76fbf065c35144d6e
Sanders Gutiérrez <ing.sanders@gmail.com>
2024-12-10 01:50:41 -0300

    feat: add support to h2 and postgresql

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO experiences (id, title, company, description, start_date, end_date) VALUES (1, 'Experience #1', 'Company #1', 'description of experience #1', '2024-11-21', '2024-12-21');
+INSERT INTO experiences (id, title, company, description, start_date, end_date) VALUES (2, 'Experience #2', 'Company #2', 'description of experience #2', '2024-12-21', '2025-01-21');
+INSERT INTO experiences (id, title, company, description, start_date, end_date) VALUES (3, 'Experience #3', 'Company #3', 'description of experience #3', '2025-01-21', '2025-02-21');
+INSERT INTO experiences (id, title, company, description, start_date, end_date) VALUES (4, 'Experience #4', 'Company #4', 'description of experience #4', '2025-02-21', '2025-03-21');
+INSERT INTO experiences (id, title, company, description, start_date, end_date) VALUES (5, 'Experience #5', 'Company #5', 'description of experience #5', '2025-03-21', '2025-04-21');
