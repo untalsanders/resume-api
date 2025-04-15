85bd362a0be124fbc2c685082a37c71f435d4223
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-15 13:02:47 -0300

    chore: adds swagger configuration
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ src/main/java/io/github/untalsanders/resume/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package io.github.untalsanders.resume.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Resume API documentation")
+                        .version("1.0.0")
+                        .description("This is the REST API documentation of the Resume backend.")
+                );
+    }
+}
---
+++ src/main/java/io/github/untalsanders/resume/rest/controller/RootRestController.java
@@ -0,0 +1,23 @@
+package io.github.untalsanders.resume.rest.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@CrossOrigin(exposedHeaders = "errors, content-type")
+@RequestMapping
+public class RootRestController {
+
+    @Value("#{servletContext.contextPath}")
+    private String servletContextPath;
+
+    @RequestMapping(value = "/")
+    public void redirectToSwagger(HttpServletResponse response) throws IOException {
+        response.sendRedirect(this.servletContextPath + "/swagger-ui/index.html");
+    }
+}
