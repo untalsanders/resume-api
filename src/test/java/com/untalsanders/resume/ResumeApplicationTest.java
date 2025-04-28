b7ef0cd7767ee09d052bc04288483247be97d92a
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-04-28 19:41:02 -0300

    chore: add integration test for ResumeApplication context loading

---
+++ src/test/java/com/untalsanders/resume/ResumeApplicationTest.java
@@ -0,0 +1,10 @@
+package com.untalsanders.resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ResumeApplicationTest {
+    @Test
+    void contextLoads() {}
+}
